58d80b3fc4b981b8b726e9a7d9d763f75bec2210
Opemipo Disu <46439130+coderoflagos@users.noreply.github.com>
2023-02-14 13:30:39 +0100

    Commit all

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+
+<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5">
+  <div class="container flex flex-wrap items-center justify-between mx-auto">
+  <a href="javascript:void(0)" class="flex items-center logo">
+      <img src="/src/assets/afriklogo.png" class="" height="40px" width="" alt="Flowbite Logo" />
+  </a>
+  <div class="flex md:order-2">
+  <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Get started</button>
+      
+
+  </div>
+
+  </div>
+</nav>
+
+
   )
 }
