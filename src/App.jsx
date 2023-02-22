d71415ca75c1990a8607d22f7d4a6a4c6a8d6caf
Opemipo Disu <46439130+coderoflagos@users.noreply.github.com>
2023-02-22 18:57:41 +0100

    Added Sign In and improvments to Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Home/homepage";
-import MovieList from "./pages/Movies/movieList"
+import SignIn from "./pages/SignIn/signin";
 
 function App() {
   return (
     <>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="movie" element={<MovieList />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+
+      <>
+      <Routes>
+        <Route path="/SignIn" element={<SignIn />} />
+      </Routes>
+
+      </>
     </>
   );
 }
