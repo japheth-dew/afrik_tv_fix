10f7d2f5e8e76e4e537a0bf3311d88445d0b718a
Temidire <temidireowoeye@gmail.com>
2023-03-13 20:28:24 +0100

    fixed and added nav-bar to profile pages

--- src/pages/Profile/chooseprofile.jsx
+++ src/pages/Profile/chooseprofile.jsx
@@ -1,46 +1,48 @@
-import React from "react";
+import React from 'react'
 // import AddIcon from '@mui/icons-material/Add';
-import AddIcon from "@mui/icons-material/Add";
-import { Link } from "react-router-dom";
+import Navbar from '../../components/Navbar'
+import AddIcon from '@mui/icons-material/Add'
+import { Link } from 'react-router-dom'
 // import Select from "react-dropdown-select";
 
 function Chooseprofile() {
-  return (
-    <div>
-      <div className="flex flex-col justify-center items-center h-screen">
-        <div className="mb-16 text-3xl font-black">Choose Profile</div>
-        <div className="grid md:grid-cols-3 grid-cols-2 gap-16 w-[80%] justify-items-center">
-          <Link to={"/"}>
-            <div>
-              <div className="w-52 h-48 bg-gray-900 flex justify-center items-center text-4xl">
-                😊
-              </div>
-              <p className="text-3xl text-center font-bold mt-4">Obiabo</p>
-            </div>
-          </Link>
-          <Link to={"/"}>
-            <div>
-              <div className="w-52 h-48 bg-[#F39C11] hover:bg-[#6a4407] flex justify-center items-center text-4xl ">
-                🤡
-              </div>
-              <p className="text-3xl text-center font-bold mt-4">Disu</p>
-            </div>
-          </Link>
-          <div className="w-52 h-48 bg-inherit flex justify-center items-center text-4xl">
-            <Link to={"/profile/new"}>
-              <div>
-                <div className="text-center ">
-                  <AddIcon style={{ fontSize: 60 }} />
-                </div>
+	return (
+		<div>
+			<Navbar />
+			<div className="flex flex-col justify-center items-center mt-10">
+				<div className="mb-16 text-3xl font-black">Choose Profile</div>
+				<div className="grid md:grid-cols-3 grid-cols-2 gap-16 w-[80%] justify-items-center">
+					<Link to={'/profile/new'}>
+						<div>
+							<div className="w-52 h-48 bg-gray-900 flex justify-center items-center text-4xl hover:bg-gray-700">
+								😊
+							</div>
+							<p className="text-3xl text-center font-bold mt-4">Obiabo</p>
+						</div>
+					</Link>
+					<Link to={'/profile/new'}>
+						<div>
+							<div className="w-52 h-48 bg-[#F39C11] hover:bg-[#6a4407] flex justify-center items-center text-4xl ">
+								🤡
+							</div>
+							<p className="text-3xl text-center font-bold mt-4">Disu</p>
+						</div>
+					</Link>
+					<div className="w-52 h-48 bg-inherit flex justify-center items-center text-4xl">
+						<Link to={'/profile/new'}>
+							<div>
+								<div className="text-center ">
+									<AddIcon style={{ fontSize: 60 }} />
+								</div>
 
-                <p className="text-base text-center font-bold">Add user</p>
-              </div>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+								<p className="text-base text-center font-bold">Add user</p>
+							</div>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default Chooseprofile;
+export default Chooseprofile
--- src/pages/Profile/manageprofile.jsx
+++ src/pages/Profile/manageprofile.jsx
@@ -1,32 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import AddIcon from '@mui/icons-material/Add';
-import AddIcon from '@mui/icons-material/Add'
+import Navbar from '../../components/Navbar'
 import { Link } from 'react-router-dom'
+import EditIcon from '@mui/icons-material/Edit'
 // import Select from "react-dropdown-select";
 
 function Manageprofile() {
+	const [hover, setHover] = useState(false)
+	const [hover2, setHover2] = useState(false)
 	return (
 		<div>
-			<div className="flex flex-col justify-center items-center h-screen">
-				<div className="mb-16 text-3xl font-black">Choose Profile</div>
-				<div className="grid md:grid-cols-3 grid-cols-2 gap-16 w-[80%] justify-items-center">
-					<Link to={'/'}>
+			<Navbar />
+			<div className="flex flex-col justify-center items-center mt-10">
+				<div className="mb-16 text-3xl font-black">Manage Profile</div>
+				<div className="flex gap-16 w-[80%] justify-center">
+					<Link to={'/profile/profileedit'}>
 						<div>
-							<div className="w-52 h-48 bg-gray-900 flex justify-center items-center text-4xl">
-								😊
+							<div
+								className="w-52 h-48 flex justify-center items-center text-4xl"
+								onMouseOver={() => {
+									setHover2(true)
+								}}
+								onMouseOut={() => {
+									setHover2(false)
+								}}
+							>
+								<div className="w-full h-full bg-gray-900 hover:opacity-80"></div>
+								<p className={`absolute ${hover2 && 'opacity-60'}`}>😊</p>
+								{hover2 && (
+									<EditIcon
+										className="absolute"
+										style={{ color: 'white', width: '60px' }}
+									/>
+								)}
 							</div>
 							<p className="text-3xl text-center font-bold mt-4">Obiabo</p>
 						</div>
 					</Link>
-					<Link to={'/'}>
+					<Link to={'/profile/profileedit'}>
 						<div>
-							<div className="w-52 h-48 bg-[#F39C11] hover:bg-[#6a4407] flex justify-center items-center text-4xl ">
-								🤡
+							<div
+								className="w-52 h-48 flex justify-center items-center text-4xl "
+								onMouseOver={() => {
+									setHover(true)
+								}}
+								onMouseOut={() => {
+									setHover(false)
+								}}
+							>
+								<div className="w-full h-full bg-[#F39C11] hover:bg-[#6a4407] "></div>
+								<p className={`absolute ${hover && 'opacity-60'}`}> 🤡</p>
+								{hover && (
+									<EditIcon
+										className="absolute"
+										style={{ color: 'white', width: '60px' }}
+									/>
+								)}
 							</div>
 							<p className="text-3xl text-center font-bold mt-4">Disu</p>
 						</div>
 					</Link>
-					<div className="w-52 h-48 bg-inherit flex justify-center items-center text-4xl">
+					{/* <div className="w-52 h-48 bg-inherit flex justify-center items-center text-4xl">
 						<Link to={'/profile/new'}>
 							<div>
 								<div className="text-center ">
@@ -35,7 +69,7 @@ function Manageprofile() {
 								<p className="text-base text-center font-bold">Add user</p>
 							</div>
 						</Link>
-					</div>
+					</div> */}
 				</div>
 			</div>
 		</div>
