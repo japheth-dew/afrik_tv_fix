9efeb6c511e4f63aa13863ce1ddd356e340ee755
Opemipo Disu <46439130+coderoflagos@users.noreply.github.com>
2023-03-13 20:39:53 +0100

    Updated error page

--- src/pages/404/error.jsx
+++ src/pages/404/error.jsx
@@ -20,13 +20,13 @@ const Nop = () => {
 				</div>
 			</nav>
 
-		<section className="bg-white dark:bg-gray-900">
+		<section className="bg-white dark:bg-white-900">
 			<div className="py-8 px-4 error-card mx-auto max-w-screen-xl lg:py-16 lg:px-6">
 				<div className="mx-auto max-w-screen-sm text-center">
 					<h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
 						404
 					</h1>
-					<p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+					<p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">
 						Aww, you're lost 😅
 					</p>
 					<p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
