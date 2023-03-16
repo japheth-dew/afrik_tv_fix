501d6175adfe16e544585cb63900e190ec94b3ea
Opemipo Disu <46439130+coderoflagos@users.noreply.github.com>
2023-03-16 12:29:17 +0100

    Added drop down without JavaScript

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,54 @@
 import React from 'react'
 
+
 function Navbar() {
 	return (
-		<nav className="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 bg-white/95">
-			<div className="max-w-8xl mx-auto">
-				<div className="py-6 lg:px-8 mx-4 lg:mx-0">
-					<img
-						src="/src/assets/afrik.png"
-						className=""
-						height="40px"
-						width=""
-						alt="AfrikTV Logo"
-					/>
+		<nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5">
+				<div className="container flex flex-wrap items-center justify-between mx-auto">
+					<a href="javascript:void(0)" className="flex items-center logo">
+						<img
+							src="/src/assets/afriklogo.png"
+							className=""
+							height="40px"
+							width=""
+							alt="AfrikTV Logo"
+						/>
+					</a>
+					<div className="flex md:order-2">
+	
+							{' '}
+					
+<button id="dropdownUserAvatarButton" data-dropdown-toggle="dropdownAvatar" class="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" type="button">
+    <span class="sr-only">Open user menu</span>
+    <img class="w-8 h-8 rounded-full" src="./src/assets/stassss" alt="userphoto"/>
+</button>
+
+
+<div id="dropdownAvatar" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+    <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
+      <div>Username</div>
+      <div class="font-medium truncate">Other information</div>
+    </div>
+    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownUserAvatarButton">
+      <li>
+        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Link</a>
+      </li>
+      <li>
+        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Link</a>
+      </li>
+      <li>
+        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Link</a>
+      </li>
+    </ul>
+    <div class="py-2">
+      <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Link</a>
+    </div>
+</div>
+
+					
+					</div>
 				</div>
-			</div>
-		</nav>
+			</nav>
 	)
 }
 
--- src/pages/auth/signup.jsx
+++ src/pages/auth/signup.jsx
@@ -55,7 +55,7 @@ const SignUp = () => {
 								<input
 									type="text"
 									name="floating_fname"
-									id="fname"
+									id="fullname"
 									class="block py-2.5 px-0 w-full text-sm text-black  bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 									placeholder=" "
 									required
@@ -64,26 +64,10 @@ const SignUp = () => {
 									for="floating_fname"
 									class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
 								>
-									First Name
+									Full Name
 								</label>
 							</div>
 
-							<div class="relative z-0 w-full mb-6 group">
-								<input
-									type="text"
-									name="floating_lname"
-									id="lname"
-									class="block py-2.5 px-0 w-full text-sm text-black  bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-									placeholder=" "
-									required
-								/>
-								<label
-									for="floating_lname"
-									class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-								>
-									Last Name
-								</label>
-							</div>
 
 							<div class="relative z-0 w-full mb-6 group">
 								<input
