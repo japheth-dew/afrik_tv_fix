e6f8aeb1c59a21d3ed46b6e8e3b64defda157fbf
Opemipo Disu <46439130+coderoflagos@users.noreply.github.com>
2023-03-17 13:38:28 +0100

    Add buttons

--- src/pages/Movies/moviepage.jsx
+++ src/pages/Movies/moviepage.jsx
@@ -9,9 +9,9 @@ function Moviepage() {
 			<div>
 				<Navbar />
 				<div class="bg-neutral-100 p-6 text-neutral-700 shadow-lg dark:bg-neutral-600 dark:text-neutral-200 dark:shadow-black/30">
-					<div className=" my-60 mx-4 lg:mx-8 mb-8">
+					<div className=" my-80 mx-4 lg:mx-8 mb-8">
 						<div>
-							<p className="text-5xl font-bold ">Movie Title</p>
+							<p className="text-7xl font-bold ">Movie Title</p>
 							<p className="my-2">No.1 on movies today</p>
 							<p className="mb-4 movie_des">
 								Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga
@@ -19,13 +19,10 @@ function Moviepage() {
 								repudiandae, sed delectus quisquam quod reprehenderit sunt modi
 								facere.
 							</p>
-							<div className="w-40 flex justify-between">
-								<button className="bg-white text-black p-2 border border-black rounded">
-									Play
-								</button>
-								<button className="bg-white text-black p-2 border border-black rounded">
-									More Info
-								</button>
+							<div className="flex ">
+							<button type="button" class="text-white-900 bg-white border movie-btn focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-800 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Play</button>
+							<button type="button" class="text-white-900 bg-white border movie-btn focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-800 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Watch Later</button>
+
 							</div>
 						</div>
 						<div></div>
