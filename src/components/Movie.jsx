e48339832a4b18cda2013098d95e33a933b17ae3
Temidire <temidireowoeye@gmail.com>
2023-03-21 14:12:11 +0100

    deleted movie component

--- src/components/Movie.jsx
+++
@@ -1,8 +0,0 @@
-export default function Movie({ title, image }) {
-  return (
-    <div className="flex flex-col gap-2">
-      <img src={image} alt="Movie image" className="w-32 h-36 rounded-md" />
-      <div className="text-sm">{title}</div>
-    </div>
-  )
-}
