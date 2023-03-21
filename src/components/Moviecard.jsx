52bca416faa7304ef6ce6bba9438db1b752ac27e
Temidire <temidireowoeye@gmail.com>
2023-03-21 09:11:13 +0100

    updated movie card

--- src/components/Moviecard.jsx
+++ src/components/Moviecard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./styles.css"
-// import woman from '../assets/woman-touch.png'
+// import Modal from "./Modal"
 
 // function Moviecard({ title, shows }) {
 // 	return (
@@ -25,27 +25,11 @@ import "./styles.css"
 
 function Moviecard({title}) {
 	return(
-		<div className='ml-5 mt-12'>
+		<div className='ml-5 mt-12 overflowhidden'>
 		<h2 className='text-4xl font-semibold'>{title}</h2>
 		<div className='flex overflow-y-hidden overflow-x-scroll p-5 card__posters transition-transform '>
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
-		<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="" />
+		{Array(15).fill().map((_, i) => (<img className='h-[150px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100 object-contain' src="https://i.pinimg.com/474x/9a/b3/b5/9ab3b59541a9e257306d53170968f5f4.jpg" alt="Movie Image" />))}
+		{/* <Modal /> */}
 			
 			{/* <div className='h-[150px] min-w-[250px] mr-2.5 duration-500 hover:scale-105 hover:opacity-100  bg-[orange]'></div> */}
 			
