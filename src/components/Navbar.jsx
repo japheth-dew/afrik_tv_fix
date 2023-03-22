7b2aed8d082af1599b957a76eed30c82b1f8737d
odelolajosh <oladapoodelola@gmail.com>
2023-03-22 16:40:18 +0100

    feat: impl dropdown and modals

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,9 @@
-import { Button, Menu, MenuItem } from '@mui/material'
-import React, { useState } from 'react'
+import { Popover, Transition, Menu } from '@headlessui/react'
+import React, { Fragment } from 'react'
 import useMenu from '../hooks/useMenu'
 import Icon from './Icon'
 
 const ITEM_HEIGHT = 80;
-const PAPER_PROPS = {
-	style: {
-		maxHeight: ITEM_HEIGHT * 4.5,
-	},
-	elevation: 0,
-	sx: {
-		boxShadow: '0px 4px 34px 0px rgba(117, 121, 132, 0.19)',
-		mt: 1.5,
-		'& .MuiAvatar-root': {
-			width: 32,
-			height: 32,
-			ml: -0.5,
-			mr: 1,
-		},
-		'&:before': {
-			content: '""',
-			display: 'block',
-			position: 'absolute',
-			top: 0,
-			right: 14,
-			width: 10,
-			height: 10,
-			bgcolor: 'background.paper',
-			transform: 'translateY(-50%) rotate(45deg)',
-			zIndex: 0,
-		},
-	},
-}
 
 function Navbar() {
 	const [openBell, bellMenu, handleBellClick, handleBellClose] = useMenu();
@@ -49,94 +21,125 @@ function Navbar() {
 						alt="AfrikTV Logo"
 					/>
 				</span>
-
-				
-				<div class="flex md:order-2">
-
-			
-		
-
-
-					<div>
-
-    
-
-
-
-						
-						<button
-							id="basic-button"
-							className="p-0"
-							aria-controls={openBell ? 'basic-menu' : undefined}
-							aria-haspopup="true"
-							aria-expanded={openBell ? 'true' : undefined}
-							onClick={handleBellClick}
-						>
-							<Icon name="bell" />
-						</button>
-						<Menu
-							id="basic-menu"
-							anchorEl={bellMenu}
-							open={openBell}
-							onClose={handleBellClose}
-							MenuListProps={{ 'aria-labelledby': 'basic-button' }}
-							className="shadow px-5 py-4"
-							PaperProps={PAPER_PROPS}
-						>
-							<div className="flex flex-col gap-4">
-								{new Array(5).fill(0).map((_, i) => (
-									<div className="flex px-5 py-2 gap-4 cursor-pointer hover:bg-[#f5f5f5]" key={i}>
-										<img src="/src/assets/venge.png" className="w-20 h-20 rounded-xl" />
-										<div className="flex flex-col">
-											<h2 className="font-light">Reminder: new arrival</h2>
-											<h2>Venge</h2>
-											<small className="mt-auto font-light">2 hrs ago</small>
-										</div>
+				<div class="flex md:order-2 gap-6">
+					{/* Notifications */}
+					{/* <Popover className="relative">
+						<>
+							<Popover.Button className="focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+								<Icon name="bell" className="h-8 w-8" />
+							</Popover.Button>
+							<Transition
+								as={Fragment}
+								enter="transition ease-out duration-200"
+								enterFrom="opacity-0 translate-y-1"
+								enterTo="opacity-100 translate-y-0"
+								leave="transition ease-in duration-150"
+								leaveFrom="opacity-100 translate-y-0"
+								leaveTo="opacity-0 translate-y-1"
+							>
+								<Popover.Panel className="absolute -right-6 z-10 mt-3 w-screen max-w-[calc(100vw-4rem)] transform px-4 sm:px-0 lg:max-w-lg">
+									<div className="bg-white overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+										{new Array(5).fill(0).map((_, i) => (
+											<div className="flex px-5 py-2 gap-4 cursor-pointer hover:bg-[#f5f5f5]" key={i}>
+												<img src="/src/assets/venge.png" className="w-20 h-20 rounded-xl" />
+												<div className="flex flex-col">
+													<h2 className="font-light">Reminder: new arrival</h2>
+													<h2>Venge</h2>
+													<small className="mt-auto font-light">2 hrs ago</small>
+												</div>
+											</div>
+										))}
 									</div>
+								</Popover.Panel>
+							</Transition>
+						</>
+					</Popover> */}
+					<Menu as="div" className="relative inline-block text-left">
+						<Menu.Button className="focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+							<Icon name="bell" className="h-8 w-8" />
+						</Menu.Button>
+						<Transition
+							as={Fragment}
+							enter="transition ease-out duration-100"
+							enterFrom="transform opacity-0 scale-95"
+							enterTo="transform opacity-100 scale-100"
+							leave="transition ease-in duration-75"
+							leaveFrom="transform opacity-100 scale-100"
+							leaveTo="transform opacity-0 scale-95"
+						>
+							<Menu.Items className="absolute -right-6 z-10 mt-3 w-[450px] max-w-[calc(100vw-4rem)] overflow-y-auto max-h-[450px] px-4 sm:px-0 lg:max-w-lg rounded bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+								{new Array(5).fill(0).map((_, i) => (
+									<Menu.Item key={i}>
+										{({ active }) => (
+											<div role="button" className={`${active ? 'bg-[#f5f5f5]' : ''} flex px-5 py-2 gap-4 cursor-pointer hover:bg-[#f5f5f5] w-full`}>
+												<img src="/src/assets/venge.png" className="w-20 h-20 rounded-xl" />
+												<div className="flex flex-col">
+													<h2 className="font-light">Reminder: new arrival</h2>
+													<h2>Venge</h2>
+													<small className="mt-auto font-light">2 hrs ago</small>
+												</div>
+											</div>
+										)}
+									</Menu.Item>
 								))}
-							</div>
-						</Menu>
-					</div>
-					<div>
-						<button
-							id="basic-button"
-							aria-controls={openUser ? 'basic-menu' : undefined}
-							aria-haspopup="true"
-							aria-expanded={openUser ? 'true' : undefined}
-							onClick={handleUserClick}
-						>
+							</Menu.Items>
+						</Transition>
+					</Menu>
+					{/* Account */}
+					<Menu as="div" className="relative inline-block text-left">
+						<Menu.Button className="focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
 							<img className="w-8 h-8 rounded-lg" src="/src/assets/profile.png" />
-						</button>
-						<Menu
-							id="basic-menu"
-							anchorEl={userMenu}
-							open={openUser}
-							onClose={handleUserClose}
-							MenuListProps={{
-								'aria-labelledby': 'basic-button',
-							}}
-							PaperProps={PAPER_PROPS}
+						</Menu.Button>
+						<Transition
+							as={Fragment}
+							enter="transition ease-out duration-100"
+							enterFrom="transform opacity-0 scale-95"
+							enterTo="transform opacity-100 scale-100"
+							leave="transition ease-in duration-75"
+							leaveFrom="transform opacity-100 scale-100"
+							leaveTo="transform opacity-0 scale-95"
 						>
-							<MenuItem onClick={handleUserClose} className="flex gap-4 hover:bg-transparent">
-								<img src="/src/assets/clown.png" className="w-8 h-8 rounded-lg" />
-								<h2 className="font-light">Anna</h2>
-							</MenuItem>
-							<MenuItem onClick={handleUserClose} className="flex gap-4">
-								<img src="/src/assets/clown.png" className="w-8 h-8 rounded-lg" />
-								<h2 className="font-light">Immanuel</h2>
-							</MenuItem>
-							<MenuItem onClick={handleUserClose} className="flex gap-4">
-								<img src="/src/assets/clown.png" className="w-8 h-8 rounded-lg" />
-								<h2 className="font-light">Opemipo</h2>
-							</MenuItem>
-							<MenuItem onClick={handleUserClose}>My account</MenuItem>
-							<MenuItem onClick={handleUserClose}>Explore Movies</MenuItem>
-							<MenuItem onClick={handleUserClose}>Logout</MenuItem>
-						</Menu>
-					</div>
+							<Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+								<div className="px-1 py-1 ">
+									{["Ope", "Immanuel"].map((name) => (
+										<Menu.Item>
+											{({ active }) => (
+												<button
+													className={`${active ? 'bg-[#f5f5f5]' : ''} group flex gap-2 w-full items-center rounded-md px-2 py-2 text-sm`}
+												>
+													<img src="/src/assets/clown.png" className="w-8 h-7 rounded-lg" />
+													<h2 className="font-base">{name}</h2>
+												</button>
+											)}
+										</Menu.Item>
+									))}
+									<Menu.Item>
+										{({ active }) => (
+											<button
+												className={`${active ? 'bg-[#f5f5f5]' : ''} w-full rounded-md px-2 py-2 text-sm font-medium`}
+											>
+												Manage profiles
+											</button>
+										)}
+									</Menu.Item>
+								</div>
+								<div className="px-1 py-1">
+									<Menu.Item>
+										{({ active }) => (
+											<button
+												className={`${active ? 'bg-[#f5f5f5]' : ''} w-full rounded-md px-2 py-2 text-sm font-medium`}
+											>
+												Sign out
+											</button>
+										)}
+									</Menu.Item>
+								</div>
+							</Menu.Items>
+						</Transition>
+					</Menu>
 				</div>
 
-				
+
 			</div>
 		</nav>
 	)
