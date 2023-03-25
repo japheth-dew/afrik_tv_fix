f040eb876651917cdf746ee20ed1045b4a73f8ff
Opemipo Disu <46439130+coderoflagos@users.noreply.github.com>
2023-03-25 21:34:32 +0100

    force commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import Chooseprofile from './pages/Profile/chooseprofile'
 import Newprofile from './pages/Profile/newprofile'
 
 import Moviepage from './pages/Movies/moviepage'
+import Searchresults from './pages/Movies/searchresults'
 
 import Dashboard from './pages/userdashboard/billing'
 import Planform from './pages/auth/planform'
@@ -55,6 +56,7 @@ const router = createBrowserRouter(
 			<Route path="/auth/planform" element={<Planform />} />
 			<Route path="/auth/billing" element={<Billing />} />
 			<Route path="/movie/moviepage" element={<Moviepage />} />
+			<Route path="/movie/searchresults" element={<Searchresults />} />
 			<Route path="in" element={<In />}>
 				<Route path="" element={<InDashboard />} />
 				<Route path="billing" element={<InBilling />} />
---
+++ src/pages/Movies/searchresults.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import './moviestyle.css'
+import PlayArrowIcon from '@mui/icons-material/PlayArrow'
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd'
+import Search from '../../components/Searchmodal'
+import MovieCard from '../../components/Moviecard'
+
+function Searchresults() {
+}
+
+export default Searchresults
