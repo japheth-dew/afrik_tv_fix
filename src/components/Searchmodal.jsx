8f13e6720e1808294a1d922b46c86f2ff28b3435
Opemipo Disu <46439130+coderoflagos@users.noreply.github.com>
2023-03-27 13:36:45 +0100

    search modal fix

--- src/components/Searchmodal.jsx
+++ src/components/Searchmodal.jsx
@@ -62,8 +62,8 @@ export default function MyModal() {
         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <SearchIcon />
         </div>
-        <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search African Movie here..." />
-        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-800 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-black-900 border border-gray-300 rounded-lg bg-gray-50 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search African Movie here..." />
+       <a href="/movie/searchresults"><button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-800 dark:focus:ring-blue-800">Search</button></a>
     </div>
 </form>
 
