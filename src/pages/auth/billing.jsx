7702071ba5b408a7dbdc48ead2f81ffc7f0be35b
Opemipo Disu <46439130+coderoflagos@users.noreply.github.com>
2023-03-28 19:39:53 +0100

    fixed billing

--- src/pages/auth/billing.jsx
+++ src/pages/auth/billing.jsx
@@ -58,61 +58,6 @@ const billing = () => {
 							</p>
 						</div>
 
-						
-							<div className="relative z-0 w-full mb-6 group">
-								<input
-									type="card_number"
-									name="floating_cardnum"
-									id="card-number"
-									className="block py-2.5 px-0 w-full text-sm text-black  bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-									placeholder=" "
-						
-								/>
-								<label
-									htmlFor="floating_cardnum"
-									className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-								>
-									Card Number
-								</label>
-							</div>
-
-							<div className="relative z-0 w-full mb-6 group">
-								<input
-									type="text"
-									name="floating_expirydate"
-									maxLength="4"
-									pattern="[0-9]*"
-									id="expirydate"
-									className="block py-2.5 px-0 w-full text-sm text-black  bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-									placeholder=" "
-							
-								/>
-								<label
-									htmlFor="floating_expirydate"
-									className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-								>
-									Expiration Date (MM)/(YY)
-								</label>
-							</div>
-
-							<div className="relative z-0 w-full mb-6 group">
-								<input
-									type="text"
-									name="floating_expirydate"
-									maxLength="3"
-									pattern="[0-9]"
-									id="expirydate"
-									className="block py-2.5 px-0 w-full text-sm text-black  bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-									placeholder=" "
-						
-								/>
-								<label
-									htmlFor="floating_expirydate"
-									className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-								>
-									CVV Code (3 digits behind your card)
-								</label>
-							</div>
 
 							<div
 								id="toast-interactive"
