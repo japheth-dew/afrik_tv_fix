089a54fb55397df7e99e74731fe94969a07937aa
Opemipo Disu <46439130+coderoflagos@users.noreply.github.com>
2023-04-02 19:16:02 +0100

    added waitlist

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,9 +3,11 @@ import Homepage from '../pages/Home/homepage'
 import Nop from '../pages/404/error'
 import { protectedRoutes } from './protected'
 import { publicRoutes } from './public'
+import Waitlist from '../pages/Waitlist/wait'
 
 export const router = createBrowserRouter([
-	{ path: '/', element: <Homepage /> },
+	{ path: '/', element: <Waitlist /> },
+	{ path: '/home/homepage', element: <Homepage /> },
 	...protectedRoutes,
 	...publicRoutes,
 	{ path: '*', element: <Nop /> },
