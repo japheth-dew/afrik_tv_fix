5a72a505502ccdeb5f6cdcec9796eb5cb1ffb260
Opemipo Disu <46439130+coderoflagos@users.noreply.github.com>
2023-04-13 10:12:31 +0100

    fixing logo height

--- src/pages/Waitlist/wait.jsx
+++ src/pages/Waitlist/wait.jsx
@@ -9,7 +9,7 @@ export default function App() {
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="" class="flex items-center">
-        <img src={logoImage} class="h-8 mr-3" alt="Flowbite Logo" />
+        <img src={logoImage} height="40px" alt="Flowbite Logo" />
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
     </a>
    
