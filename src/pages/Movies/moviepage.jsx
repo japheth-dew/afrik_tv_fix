ad5ac4c7c6849042af0f645970ff15b8f0fc7356
Temidire <temidireowoeye@gmail.com>
2023-04-14 12:22:32 +0100

    fix: play now, watch later button

--- src/pages/Movies/moviepage.jsx
+++ src/pages/Movies/moviepage.jsx
@@ -24,7 +24,7 @@ function Moviepage() {
 								type="button"
 								className="text-white-900 bg-white border movie-btn focus:outline-none focus:ring-4 focus:ring-gray-200 font-medium text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-800  dark:hover:border-gray-800 dark:focus:ring-gray-700"
 							>
-								<PlayArrowIcon />
+								<PlayArrowIcon className='m-0'/>
 								Play Now
 							</button>
 							<button
@@ -32,7 +32,7 @@ function Moviepage() {
 								className="text-white-900 bg-white border movie-btn focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-800 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
 							>
 								{' '}
-								<PlaylistAddIcon />
+								<PlaylistAddIcon className='m-0'/>
 								Watch Later
 							</button>
 						</div>
